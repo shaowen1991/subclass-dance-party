efcdad6cd426914df45f15d9db909266bf6c4f0d
Hack Reactor Student <student@hackreactor.com>
2017-04-03 17:32:01 -0700

    Partially workthrough

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,24 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
+  
+  
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-  this.oldStep = makeDancer.prototype.step.bind(this);
+  this.oldStep = makeDancer.prototype.step;
+  
 };
 
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
-
-// makeBlinkyDancer.prototype.oldStep = makeDancer.prototype.step;
+makeBlinkyDancer.prototype.oldStep = function() {
+  makeDancer.prototype.step();
+}
 makeBlinkyDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
   // this.oldStep();
-  return this.oldStep();
+  
+  this.oldStep();
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
--- src/dancer.js
+++ src/dancer.js
@@ -3,10 +3,17 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
+  this.top = top;
+  this.left = left;
+  this.timeBetweenSteps = timeBetweenSteps;
+
+
   this.step();
+  this.setPosition(this.top, this.left);
+  // makeDancer.prototype.step();
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  this.setPosition(top, left);
+  // makeDancer.prototype.setPosition(top, left);
 };
 
 makeDancer.prototype.step = function() {
@@ -23,5 +30,7 @@ makeDancer.prototype.setPosition = function(top, left) {
     top: top,
     left: left
   };
+  // debugger
+  // this
   this.$node.css(styleSettings);
 };
