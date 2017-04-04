35d855d51920f364cd5c782857084a4a67e6c0e5
Hack Reactor Student <student@hackreactor.com>
2017-04-03 19:57:19 -0700

    updated spec and remove comments from blinky

--- src/dancer.js
+++ src/dancer.js
@@ -19,7 +19,7 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 makeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  this
+  
   setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
@@ -33,5 +33,6 @@ makeDancer.prototype.setPosition = function(top, left) {
   };
   // debugger
   // this
+
   this.$node.css(styleSettings);
 };
